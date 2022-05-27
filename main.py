63cad13d946ec42967282984f9bc35900f1f1c23
Neuroth, Brad <neurothbr@epb.net>
2022-05-27 15:29:21 -0400

    use the nextcloud client so stuff does not get lost

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 from dotenv import dotenv_values
 
+import nextcloud_client
+
 def download_attachments():
     config = dotenv_values('/usr/local/sbin/imap-to-nextcloud/.env')
     with imaplib.IMAP4_SSL(host=config['HOST'], port=993) as imap_ssl:
@@ -20,15 +22,24 @@ def download_attachments():
             subject = message.get('Subject')
             print(subject)
             if '***REMOVED***' in subject:
-                path = config['BASEPATH'] + '***REMOVED***/files/Photos/Phone_Uploads/'
+                path = config['BASEPATH'] + '/Photos/Phone_Uploads/'
             elif '***REMOVED***' in subject:
-                path = config['BASEPATH'] + '***REMOVED***/files/Photos/Phone_Uploads/'
+                path = config['BASEPATH'] + '/Photos (***REMOVED***)/Phone_Uploads/'
             for part in message.walk():
                 filename = part.get_filename() if part.get_filename() else str(datetime.now())+'.jpg'
                 if part.get_content_maintype() != 'multipart' and part.get('Content-Disposition') is not None:
-                    open(path + filename, 'wb').write(part.get_payload(decode=True))
+                    add_to_nextcloud(config, part, user, path+filename)
             imap_ssl.copy(mail_id, 'Processed_Photos')
             imap_ssl.store(mail_id, '+FLAGS', '\Deleted')
+
+
+def add_to_nextcloud(config, part, user, filename):
+    nc = nextcloud_client.Client(config['NEXTCLOUD_DOMAIN'])
+
+    nc.login(config['NEXTCLOUD_USER'], config['NEXTCLOUD_PASSWORD'])
+
+    nc.put_file_contents(filename, part.get_payload(decode=True))
+
 
 
 # Press the green button in the gutter to run the script.
